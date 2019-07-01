3b53bacf48a632bfaafe888301240437f6bb569d
Sven Korset <sven.korset@indie-software.de>
2019-07-01 15:22:24 +0200

    Release target renamed into production

--- DemoApp/Pods/Target Support Files/Pods-DemoAppProd/Pods-DemoAppProd-umbrella.h
+++ DemoApp/Pods/Target Support Files/Pods-DemoAppProd/Pods-DemoAppProd-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_DemoAppReleaseVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_DemoAppReleaseVersionString[];
+FOUNDATION_EXPORT double Pods_DemoAppProdVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_DemoAppProdVersionString[];
 
--- DemoApp/Pods/Target Support Files/Pods-DemoAppRelease/Pods-DemoAppRelease-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DemoAppRelease : NSObject
-@end
-@implementation PodsDummy_Pods_DemoAppRelease
-@end
